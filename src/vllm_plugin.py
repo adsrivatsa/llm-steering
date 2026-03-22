4b3e528b0cbccf95912b6c9c46fb9bdf43da193b
Abhinav Srivatsa <asrivatsa6@gmail.com>
2026-03-22 06:28:15 +0000

    added nodels and uv update

--- src/vllm_plugin.py
+++ src/vllm_plugin.py
@@ -12,11 +12,23 @@ def register_activation_models():
     from vllm.model_executor.models import ModelRegistry
 
     ModelRegistry.register_model(
-        "GptOssForCausalLM", "src.activation.modelling.gptmoe20b:GptOssForCausalLM"
+        "GptOssForCausalLM", "src.activation.modelling.gpt:GptOssForCausalLM"
     )
 
     ModelRegistry.register_model(
-        "OlmoeForCausalLM", "src.activation.modelling.olmoe7b:OlmoeForCausalLM"
+        "OlmoeForCausalLM", "src.activation.modelling.olmoe:OlmoeForCausalLM"
+    )
+
+    ModelRegistry.register_model(
+        "Qwen3MoeForCausalLM", "src.activation.modelling.qwen:Qwen3MoeForCausalLM"
+    )
+
+    ModelRegistry.register_model(
+        "MixtralForCausalLM", "src.activation.modelling.mixtral:MixtralForCausalLM"
+    )
+
+    ModelRegistry.register_model(
+        "PhiMoEForCausalLM", "src.activation.modelling.phi:PhiMoEForCausalLM"
     )
 
 
